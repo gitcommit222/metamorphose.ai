e14d5b9ae0b8eaa77534ad0eab6c1517c5d22b54
gitcommit222 <TiomarOne02@gmail.com>
2024-03-10 01:40:03 +0800

    DB NAME UPDATED

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -24,7 +24,7 @@ export const connectToDatabase = async () => {
 	cached.promise =
 		cached.promise ||
 		mongoose.connect(MONGODB_URL, {
-			dbName: "metamorphose.ai",
+			dbName: "metamorphose-ai",
 			bufferCommands: false,
 		});
 
