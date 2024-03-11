e247adec72aa5d7c12ad17bd67a19a35b946aec1
gitcommit222 <TiomarOne02@gmail.com>
2024-03-11 22:57:06 +0800

    Inputs and other components

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,32 @@
+import Header from "@/components/shared/Header";
 import React from "react";
+import { transformationTypes } from "@/constants";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { auth } from "@clerk/nextjs";
+import { getUserById } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 
-const AddTransformationType = () => {
-	return <div>AddTransformationType</div>;
+const AddTransformationType = async ({
+	params: { type },
+}: SearchParamProps) => {
+	const { userId } = auth();
+	const transformation = transformationTypes[type];
+
+	if(!userId) redirect("/sign-in");
+
+	const user = await getUserById(userId);
+
+	return (
+		<>
+			<Header title={transformation.title} subtitle={transformation.subTitle} />
+			<TransformationForm
+				action="Add"
+				userId={user._id}
+				type={transformation.type as TransformationTypeKey}
+				creditBalance={user.creditBalance}
+			/>
+		</>
+	);
 };
 
 export default AddTransformationType;
