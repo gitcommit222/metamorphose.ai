ec3937ac017e7aaf2db38861b4d552f03ce2ad4f
gitcommit222 <TiomarOne02@gmail.com>
2024-06-16 23:18:02 +0800

    Final Deployment

--- app/(root)/credits/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CreditPage = () => {
-  return (
-    <div>Credits</div>
-  )
-}
-
-export default CreditPage
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,14 +6,11 @@ import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<main className="root relative">
+		<main className="root">
 			<Sidebar />
 			<MobileNav />
 			<div className="root-container">
 				<div className="wrapper">{children}</div>
-				{/* <div className="w-full shadow-lg fixed bottom-0 bg-white mr-[50%]">
-					<Footer />
-				</div> */}
 			</div>
 			<Toaster />
 		</main>
